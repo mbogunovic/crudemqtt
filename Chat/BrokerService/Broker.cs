f1578f8f9dcd2b51e1e8308efaedd075f25f49c2
Maksim Bogunović <m.bogunovic@vegaitsorucing.rs>
2019-01-09 16:06:35 +0100

    Added Broker as a service to project

---
+++ Chat/BrokerService/Broker.cs
@@ -0,0 +1,20 @@
+﻿using uPLibrary.Networking.M2Mqtt;
+
+namespace Chat.BrokerService
+{
+	internal class Broker
+	{
+		private MqttBroker service { get; }
+
+		internal Broker()
+		{
+			service = new MqttBroker();
+		}
+
+		internal void Initialize() =>
+			this.service.Start();
+
+		internal void Stop() =>
+			this.service.Stop();
+	}
+}
---
+++ Chat/BrokerService/Service.cs
@@ -0,0 +1,25 @@
+﻿using System.ServiceProcess;
+
+namespace Chat.BrokerService
+{
+	partial class Service : ServiceBase
+	{
+		private Broker broker;
+
+		public Service()
+		{
+			InitializeComponent();
+		}
+
+		protected override void OnStart(string[] args)
+		{
+			broker = new Broker();
+			broker.Initialize();
+		}
+
+		protected override void OnStop()
+		{
+			broker.Stop();
+		}
+	}
+}
